33165a5fa2ee4941ba778603881371dd2c5afebc
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-01-31 18:08:12 +0530

    downloaded depencies

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// bootstrap 
+
+function App() {
+  
+
+  return (
+   <div></div> 
+  )
+}
+
+export default App
