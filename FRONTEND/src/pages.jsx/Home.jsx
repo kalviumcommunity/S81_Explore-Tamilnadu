5caf2d403f57a247cbb94f2dcbce20685609a62a
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-02-04 17:02:19 +0530

    done

--- FRONTEND/src/pages.jsx/Home.jsx
+++ FRONTEND/src/pages.jsx/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-
 function Home() {
     return (
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-6xl font-semibold flex items-center">
-          
-          Welcome to Home
+      <div 
+        className="flex items-center justify-center h-screen bg-cover bg-center" 
+        style={{ backgroundImage: "url(https://www.tamilnadutourism.tn.gov.in/img/pages/large-desktop/kanyakumari-1654195435_bab3b5c9e1fc90ab56a8.webp)", paddingTop: "350px" }}
+      >
+        <h1 
+          className="text-5xl font-semibold flex items-center p-4 rounded text-background-clip"
+          style={{ backgroundImage: "url(https://www.tamilnadutourism.tn.gov.in/img/pages/large-desktop/kanyakumari-1654195435_bab3b5c9e1fc90ab56a8.webp)" }}
+        >
+          WELCOME TO TAMILNADU
         </h1>
       </div>
     );
