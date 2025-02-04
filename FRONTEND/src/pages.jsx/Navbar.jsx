5c582ace2042dda08468f0d9d323d02f73f3b2f6
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-02-04 17:49:38 +0530

    done

--- FRONTEND/src/pages.jsx/Navbar.jsx
+++ FRONTEND/src/pages.jsx/Navbar.jsx
@@ -4,7 +4,7 @@ import { TbLogin2 } from "react-icons/tb";
 
 function Navbar() {
   return (
-    <ul className="fixed top-0 right-[200px] p-4">
+    <ul className="absolute top-0 right-[200px] p-4">
       <li className="inline-block mr-4 mt-10">
         <Link
           to="/"
