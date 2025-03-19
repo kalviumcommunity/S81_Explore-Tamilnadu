5b202c59edaa7a9af1a21fe6bb20792066fa56e9
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-03-19 10:53:17 +0530

    done

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -4,11 +4,6 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-// Add /ping route
-app.get("/ping", (req, res) => {
-    res.send("Pong!");
-});
-
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
